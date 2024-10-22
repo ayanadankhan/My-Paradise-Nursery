52b1410699a262323af790932c7f7bf0f8aef23d
ayanadankhan <ayanadankhan@gmail.com>
2024-10-22 12:05:38 +0500

    updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -38,9 +38,12 @@ function LandingPage() {
         <div className="col-12 col-md-12 text-center">
           <h1 className="display-3 text-white">Welcome to Paradise Nursery</h1>
           <p className="lead text-white">Explore our wide variety of houseplants to bring life into your home.</p>
-          <a href="/products">
-            <button className="btn btn-primary btn-lg mt-3">Get Started</button>
-          </a>
+        
+            <button className="btn btn-primary btn-lg mt-3">
+            <Link className="nav-link" to="/products">Get Started</Link>
+            </button>
+          
+          
         </div>
       </div>
     </div>
